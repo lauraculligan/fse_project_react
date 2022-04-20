b43dcb54f073529ed8730026d874a622d0c70dc0
Laura Culligan <lgculligan@gmail.com>
2022-04-19 23:04:19 -0400

    search on change and minor formatting updates

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -2,21 +2,40 @@
 import React, {useState} from "react";
 import * as usersService from "../../services/users-service";
 import {useNavigate} from "react-router-dom";
-import Tuit from "../tuits/tuit";
 import SearchResults from "./search";
 
 const Search = () => {
     const navigate = useNavigate();
-    const [username, setUsername] = useState("");
     const [foundUsers, setFoundUsers] = useState({});
 
-    const searchUsers = async () => {
-        await usersService.searchUserByName(username)
-            .then(foundUsers => setFoundUsers(foundUsers));
+    const searchUsers = async (name) => {
+        if (name != "") {
+            await usersService.searchUserByName(name)
+                .then(foundUsers => setFoundUsers(foundUsers));
+        }
+        else {
+            setFoundUsers({});
+        }
     }
 
     return (<>
         <h1>Search Screen</h1>
+
+        <div className="p-2 w-100">
+            <div className="row align-items-center">
+
+                <i className="fas ps-4 fa-search position-absolute"></i>
+                <div className="col-10 ttr-font-size-100pc border-2 text-primary">
+                   <textarea
+                       onChange={(e) =>
+                           searchUsers(e.currentTarget.value)}
+
+                       placeholder="Search Users"
+                       className="w-100 ps-5 border-2 rounded-pill form-control align-center"
+                   ></textarea>
+                </div>
+            </div>
+        </div>
             <div>
                 <ul className="ttr-results list-group">
                     {
@@ -24,27 +43,6 @@ const Search = () => {
                     }
                 </ul>
             </div>
-
-        <div className="p-2 w-100">
-
-            <div className="row align-items-center">
-                <div className="col-10 ttr-font-size-100pc border-2 text-primary">
-                   <textarea
-                       onChange={(e) =>
-                           setUsername(e.target.value)}
-                       placeholder="Search Users"
-                       className="w-100 border-2"
-                   ></textarea>
-                </div>
-                <div className="col-2">
-                    <button onClick={searchUsers}
-                            className={`btn btn-primary rounded-pill fa-pull-left
-                                fw-bold ps-4 pe-4`}>
-                        Search
-                    </button>
-                </div>
-            </div>
-        </div>
         </>
     );
 };
